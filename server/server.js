b043b928e59a55126c210f336fd6577d123a3c8f
Shreya Choudhary <shreyachy510@gmail.com>
2026-05-14 22:32:53 +0530

    fixed render port issue

--- server/server.js
+++ server/server.js
@@ -32,9 +32,8 @@ app.get('/', (req, res) => {
 // Error handlers
 app.use(notFound);
 app.use(errorHandler);
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
